b9a6ecbd6de491e0d138a14a41cbee7507ec78f7
Nicole Swan <nicole.swan@bellevuecollege.edu>
2018-10-10 13:25:32 -0700

    Add filtering capability for student list view
    - Feature per JIRA ER-52
    - Adds capability to filter student view by SID, first name,
     last name, country, projected quarter, or group

--- ELI/Pages/StudentSearch.cshtml.cs
+++ ELI/Pages/StudentSearch.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ELI.Models;
@@ -19,12 +20,81 @@ namespace ELI.Pages
         }
 
         public IList<StudentSearch> StudentData { get; set; }
+        public SelectList SelectGroups { get; set; }
+        public SelectList SelectCountries { get; set; }
+        public SelectList SelectQuarters { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string LnameSearch { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string FnameSearch { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SidSearch { get; set; }
+
+        [BindProperty(SupportsGet = true)] public string GroupSearch { get; set; }
+        [BindProperty(SupportsGet = true)] public string CountrySearch { get; set; }
+        [BindProperty(SupportsGet = true)] public string QuarterSearch { get; set; }
 
         public void OnGet()
         {
             _context.Database.SetCommandTimeout(200);
-            StudentData = _context.StudentSearchResults.FromSql("EXEC dbo.usp_getFakeStudentView").ToList();
             //StudentData = _context.StudentSearchResults.FromSql("EXEC dbo.usp_getStudentViewData").ToList();
+            StudentData = _context.StudentSearchResults.FromSql("EXEC dbo.usp_getFakeStudentView").ToList();
+
+            /***
+             * get groups, countries, and quarters to fill filter drop downs
+             * need to do before student data is filtered
+            ***/
+            var g = StudentData.Select(s => s.Program).Distinct();
+            if (!String.IsNullOrEmpty(GroupSearch))
+            {
+                SelectGroups = new SelectList(g, GroupSearch);
+            }
+            else SelectGroups = new SelectList(g);
+
+            var c  = StudentData.Select(s => s.Country).Distinct();
+            if (!String.IsNullOrEmpty(CountrySearch))
+            {
+                SelectCountries = new SelectList(c, CountrySearch);
+            }
+            else SelectCountries = new SelectList(c);
+
+            var q = StudentData.Select(s => s.ProjectedQuarter).Distinct();
+            if (!String.IsNullOrEmpty(QuarterSearch))
+            {
+                SelectQuarters = new SelectList(q, QuarterSearch);
+            }
+            else SelectQuarters = new SelectList(q);
+            /*** end generate drop down filters ***/
+
+            /***
+             * Based on input search filters, filter student data
+            ***/
+            if ( !String.IsNullOrEmpty(LnameSearch) )
+            {
+                StudentData = StudentData.Where(s => s.LastName.ToLower().StartsWith(LnameSearch.ToLower())).ToList();
+            }
+            if( !String.IsNullOrEmpty(SidSearch))
+            {
+                StudentData = StudentData.Where(s => s.Sid.Equals(SidSearch)).ToList();
+            }
+            if (!String.IsNullOrEmpty(FnameSearch))
+            {
+                StudentData = StudentData.Where(s => s.FirstName.ToLower().StartsWith(FnameSearch.ToLower())).ToList();
+            }
+            if (!String.IsNullOrEmpty(GroupSearch))
+            {
+                StudentData = StudentData.Where(s => s.Program.Equals(GroupSearch)).ToList();
+            }
+            if (!String.IsNullOrEmpty(CountrySearch))
+            {
+                StudentData = StudentData.Where(s => s.Country.Equals(CountrySearch)).ToList();
+            }
+            if (!String.IsNullOrEmpty(QuarterSearch))
+            {
+                StudentData = StudentData.Where(s => s.ProjectedQuarter.Equals(QuarterSearch)).ToList();
+            }
         }
     }
 }
