a651fb95a7817879a2b32981922d9046a8a3a606
Nicole Swan <nicole.swan@bellevuecollege.edu>
2018-11-01 10:04:06 -0700

    Add authentication and authorization
    - Add a custom handler (and support classes) for authorization
    that will parse a comma-delimited list of groups authorized to use app.
    - Wire into Startup class.  Set up in such a way to not require
    authorization for development environment (to allow use on Mac side).
    - Update example settings files with necessary setting info.

---
+++ ELI/Helpers/ELIAuthorizationHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using ELI.Models;
+
+namespace ELI.Helpers
+{
+
+    public class ELIAuthorizationHandler : AuthorizationHandler<ELIAdminRequirement>
+    {
+        IOptionsSnapshot<ApplicationSettings> _appSettings;
+
+        public ELIAuthorizationHandler(IOptionsSnapshot<ApplicationSettings> appSettings)
+        {
+            _appSettings = appSettings;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ELIAdminRequirement requirement)
+        {
+            string groupsSetting = _appSettings.Value.AuthorizedGroups;
+            bool passed = false;
+
+            if (!String.IsNullOrWhiteSpace(groupsSetting))
+            {
+                var groups = groupsSetting.Split(",");
+                foreach (var group in groups)
+                {
+                    if (context.User.IsInRole(group))
+                    {
+                        //context.Succeed(requirement);
+                        passed = true;
+                        break;
+                    }
+                }
+            }
+
+            if (passed)
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+}
--- ELI/Startup.cs
+++ ELI/Startup.cs
@@ -7,21 +7,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ELI.Models;
+using ELI.Helpers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.WsFederation;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Server.IISIntegration;
 
 namespace ELI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            HostingEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,8 +40,33 @@ namespace ELI
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.Configure<ApplicationSettings>(Configuration.GetSection("ApplicationSettings"));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //configure authorization if not development environment
+            if (!HostingEnvironment.IsDevelopment())
+            {
+                services.AddAuthentication(IISDefaults.AuthenticationScheme);
+
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .AddRequirements(new ELIAdminRequirement())
+                    /*.RequireAssertion(x =>
+                                        x.User.IsInRole(@"CAMPUS\KBCS-FullTimeStaff") || 
+                                        x.User.IsInRole(@"CAMPUS\Developers")
+                    )*/
+                    .Build();
+
+                services.AddMvc(options =>
+                {
+                    options.Filters.Add(new Microsoft.AspNetCore.Mvc.Authorization.AuthorizeFilter(policy));
+                }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+                services.AddScoped<IAuthorizationHandler, ELIAuthorizationHandler>();
+            }
+            else
+            {
+                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            }
 
             services.AddDbContext<ELIContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("ELIDatabase")));
@@ -55,6 +87,7 @@ namespace ELI
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc();
