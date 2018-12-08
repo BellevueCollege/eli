20dc152f0e1de47ab4deeaaeca34265458688fcf
Nicole Swan <nicole.swan@bellevuecollege.edu>
2018-12-07 17:00:45 -0800

    Add section name variable to class
    - Related to work for Jira ER-59

--- ELI/Models/ApplicationSettings.cs
+++ ELI/Models/ApplicationSettings.cs
@@ -7,6 +7,8 @@ namespace ELI.Models
 {
     public class ApplicationSettings
     {
+        public static string SectionName = "ApplicationSettings";
         public string AuthorizedGroups { get; set; }
+        public string SessionKey_SelectedQuarter { get; set; }
     }
 }
