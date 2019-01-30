fbe1e462c67b758c108e794882055746e9663652
Nicole Swan <nicole.swan@bellevuecollege.edu>
2019-01-29 16:57:56 -0800

    Update Index to implement EliPageModel
    - Updated index page to implement EliPageModel so
    header quarter select works
    - Updated links

--- ELI/Pages/Index.cshtml.cs
+++ ELI/Pages/Index.cshtml.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ELI.Models;
 
 namespace ELI.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel : EliPageModel
     {
+        public IndexModel(ELIContext context, IConfiguration config, ILogger<IndexModel> logger) : base(context, config, logger) { }
+
         public void OnGet()
         {
 
