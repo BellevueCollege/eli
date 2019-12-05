19929cb5179e3e49bfa4642ebbd20c878a8bb317
Elizabeth Fuenzalida <efuenzalida52@gmail.com>
2019-12-05 13:06:49 -0800

    Adding a model page for Assign Class section
    Adding a model page for the assign classes functionality of this
    project.

---
+++ ELI/Models/AssignClasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELI.Models
+{
+    /**
+     * Basic model for Assign Classes object
+     * **/
+    public class AssignClasses
+    {
+        public string Sid { get; private set; }
+        public string LastName { get; private set; }
+        public string FirstName { get; private set; }
+        public string Country { get; private set; }
+     // public string New { get; private set; }
+
+    }
+}
