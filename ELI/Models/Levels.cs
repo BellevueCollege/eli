8982c030ba5e321a56ad38435df49fab53444b3d
Angela Vu <angela.vu@bellevuecollege.edu>
2020-02-07 18:03:08 -0800

    Update Levels Model to include LevelsID and Types
    - Update Levels model to adjust to new Levels table schema and include
    primary key LevelsID
    - Update to include Column Typenames
    
    Related to ER-99, ER-100.

--- ELI/Models/Levels.cs
+++ ELI/Models/Levels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,16 +10,22 @@ namespace ELI.Models
     public class Levels
     {
         [Key]
-        public string Sid { get; set; }
-        public string YearQuarterID { get; set; }
-        public int? WriteLevel { get; set; }
-        public string WriteGrade { get; set; }
-        public int? WritePlace { get; set; }
+        public int LevelID { get; set; }
+        public string SID { get; set; }
         public int? ReadLevel { get; set; }
         public string ReadGrade { get; set; }
+        [Column(TypeName = "decimal(3,2)")]
+        public decimal? ReadGradePoint { get; set; }
         public int? ReadPlace { get; set; }
+        public int? WriteLevel { get; set; }
+        public string WriteGrade { get; set; }
+        [Column(TypeName = "decimal(3,2)")]
+        public decimal? WriteGradePoint { get; set; }
+        public int? WritePlace { get; set; }
         public int? SpeakLevel { get; set; }
         public string SpeakGrade { get; set; }
+        [Column(TypeName = "decimal(3,2)")]
+        public decimal? SpeakGradePoint { get; set; }
         public int? SpeakPlace { get; set; }
     }
 }
