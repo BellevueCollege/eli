d15fc6c303a058def6e2fef23d7117d215852906
Angela Vu <angela.vu@bellevuecollege.edu>
2020-02-13 14:44:46 -0800

    Add Override Inputs to Edit Returning Placements
    - Adds inputs with number validation to Edit Returning Placements Pages
    - Adds validation range properties to Levels placements in Levels
    Model to make sure new placements are valid
    
    Working on ER-111.

--- ELI/Models/Levels.cs
+++ ELI/Models/Levels.cs
@@ -16,16 +16,19 @@ namespace ELI.Models
         public string ReadGrade { get; set; }
         [Column(TypeName = "decimal(3,2)")]
         public decimal? ReadGradePoint { get; set; }
+        [Range(1, 5)]
         public int? ReadPlace { get; set; }
         public int? WriteLevel { get; set; }
         public string WriteGrade { get; set; }
         [Column(TypeName = "decimal(3,2)")]
         public decimal? WriteGradePoint { get; set; }
+        [Range(1, 5)]
         public int? WritePlace { get; set; }
         public int? SpeakLevel { get; set; }
         public string SpeakGrade { get; set; }
         [Column(TypeName = "decimal(3,2)")]
         public decimal? SpeakGradePoint { get; set; }
+        [Range(1, 5)]
         public int? SpeakPlace { get; set; }
     }
 }
